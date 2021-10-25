52b431037c5a61025c9b31b0de5cff12460412bf
hanseom <hanseomkim@gmail.com>
2021-10-25 21:25:26 +0900

    Common Interface

---
+++ src/main/java/com/spring/data/datajpa/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package com.spring.data.datajpa.repository;
+
+import com.spring.data.datajpa.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+}
